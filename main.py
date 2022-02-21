403fe77a446cd04d2005193873374991992a7fcd
Eurin-lumen <eurindelagoldcoast@gmail.com>
2022-02-21 21:29:01 +0100

    Exo 1 : Corretion

---
+++ main.py
@@ -0,0 +1,19 @@
+# programme testant si une année, entrée par l'utilisateur,
+# est bissextile ou non
+annee = input("Entrez une année : ")
+# on attend que l'utilisateur entre l'année qu'il désire tester
+annee = int(annee) # risque d'erreur si l'utilisateur n'a pas rentré un nombre
+bissextile = False # on crée un booléen qui va être vrai ou faux
+# si l'année est bissextile ou non
+if annee%400==0:
+    bissextile = True
+elif annee%100==0:
+    bissextile = False
+elif annee%4==0:
+    bissextile = True
+else:
+    bissextile = False
+if bissextile: # si l'année est bissextile
+    print(f"L'année {annee} est bissextile.")
+else:
+    print("L'année {annee} n'est pas bissextile.")
